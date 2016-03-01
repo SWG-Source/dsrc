35fbaf22864e6e54dd4ebf8728e45847dd7dce61
Cekis <cekisswg@gmail.com>
2016-03-01 07:46:43 +0100

    Reverted unnecessary changes to the Borvo's Guards spawner.  Fixed Celebrity Spawner such that double spawns do not happen when using it incorrectly.

--- sku.0/sys.server/compiled/game/script/npc/celebrity/celebrity_spawner.java
+++ sku.0/sys.server/compiled/game/script/npc/celebrity/celebrity_spawner.java
@@ -49,33 +49,7 @@ public class celebrity_spawner extends script.base_script
     }
     public int OnAttach(obj_id self) throws InterruptedException
     {
-        String spawn = getStringObjVar(self, "spawns");
-        location here = getLocation(self);
-        here.y = here.y + 0.5f;
-        obj_id celeb = create.object(spawn, here);
-        setInvulnerable(celeb, true);
-        ai_lib.setDefaultCalmBehavior(celeb, ai_lib.BEHAVIOR_SENTINEL);
-        if (hasObjVar(self, "quest_script"))
-        {
-            String script = getStringObjVar(self, "quest_script");
-            attachScript(celeb, script);
-        }
-        if (hasObjVar(self, "quest_table"))
-        {
-            String table = getStringObjVar(self, "quest_table");
-            setObjVar(celeb, "quest_table", table);
-        }
-        if (hasObjVar(self, "npc_name"))
-        {
-            String name = getStringObjVar(self, "npc_name");
-            if (name != null)
-            {
-                if (!name.equals(""))
-                {
-                    setName(celeb, name);
-                }
-            }
-        }
+        // what the heck?  Removed a bunch of crap that wasn't in the original.  will likely break some spawns due to ignorance.
         return SCRIPT_CONTINUE;
     }
 }
