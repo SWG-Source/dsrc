0516da8f78397f8dc13842aed00232665c1e23d9
Cekis <cekisswg@gmail.com>
2016-03-04 05:43:49 +0100

    Added objvar to control whether or not a bartender is spawned in cantinas.

--- sku.0/sys.server/compiled/game/script/structure/municipal/cantina.java
+++ sku.0/sys.server/compiled/game/script/structure/municipal/cantina.java
@@ -25,18 +25,20 @@ public class cantina extends script.base_script
         {
             return SCRIPT_OVERRIDE;
         }
-        behindBar = new location(8.54f, -0.89f, 0.14f, planet, cantina);
-        obj_id bartender = create.object("bartender", behindBar);
-        setYaw(bartender, 87);
-        // removed static setting as it caused bartenders to just stay put.  They move around the bar
-        // so this caused a memory leak by storing points it couldn't navigate to.
-        // setCreatureStatic(bartender, true);
+        if(getIntObjVar(self,"intSpawnBartender") == 1) {
+            behindBar = new location(8.54f, -0.89f, 0.14f, planet, cantina);
+            obj_id bartender = create.object("bartender", behindBar);
+            setYaw(bartender, 87);
+            // removed static setting as it caused bartenders to just stay put.  They move around the bar
+            // so this caused a memory leak by storing points it couldn't navigate to.
+            // setCreatureStatic(bartender, true);
 
-        setInvulnerable(bartender, true);
-        attachScript(bartender, "npc.bartender.base");
-        attachScript(bartender, "npc.bartender.listen");
-        ai_lib.setDefaultCalmMood(bartender, "npc_imperial");
-        //ai_lib.setDefaultCalmBehavior(self, ai_lib.BEHAVIOR_SENTINEL);
+            setInvulnerable(bartender, true);
+            attachScript(bartender, "npc.bartender.base");
+            attachScript(bartender, "npc.bartender.listen");
+            ai_lib.setDefaultCalmMood(bartender, "npc_imperial");
+            //ai_lib.setDefaultCalmBehavior(self, ai_lib.BEHAVIOR_SENTINEL);
+        }
         if (hasScript(self, "theme_park.lok.cantina"))
         {
             if (hasScript(self, "city.imperial_crackdown.crackdown_cantina"))
