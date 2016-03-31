24959d439c639693b1339724cdcb6775b8b8a521
Cekis <cekisswg@gmail.com>
2016-03-31 04:24:12 +0100

    Made code efficiency adjustments to imperial crackdowns.

--- sku.0/sys.server/compiled/game/script/city/imperial_crackdown/crackdown_cantina.java
+++ sku.0/sys.server/compiled/game/script/city/imperial_crackdown/crackdown_cantina.java
@@ -1,20 +1,10 @@
 package script.city.imperial_crackdown;
 
-import script.*;
-import script.base_class.*;
-import script.combat_engine.*;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.Vector;
-import script.base_script;
-
-import script.library.utils;
-import script.library.chat;
-import script.library.ai_lib;
-import script.library.create;
-import script.library.factions;
-import script.library.gcw;
-import script.library.locations;
+import script.dictionary;
+import script.library.*;
+import script.location;
+import script.obj_id;
+import script.string_id;
 
 public class crackdown_cantina extends script.base_script
 {
@@ -48,7 +38,6 @@ public class crackdown_cantina extends script.base_script
     public int doTroubleCheck(obj_id self, dictionary params) throws InterruptedException
     {
         removeObjVar(self, "crackdownHappened");
-        obj_id player = params.getObjId("player");
         if (hasObjVar(self, "crackdownHappened"))
         {
             if (hasObjVar(self, "checkingForTrouble"))
@@ -112,8 +101,8 @@ public class crackdown_cantina extends script.base_script
                 reb_r = 1.f;
             }
         }
-        String troublemaker = null;
-        String faction = null;
+        String troublemaker;
+        String faction;
         if (imp_r >= reb_r)
         {
             troublemaker = "crackdown_stormtrooper";
@@ -124,8 +113,7 @@ public class crackdown_cantina extends script.base_script
             troublemaker = "crackdown_rebel_command_security_guard";
             faction = "rebel";
         }
-        location impLoc = new location(48.13f, .1f, 2.47f, planet, foyer);
-        obj_id trooper = create.object(troublemaker, impLoc);
+        obj_id trooper = create.object(troublemaker, new location(48.13f, .1f, 2.47f, planet, foyer));
         setObjVar(trooper, "string_faction", faction);
         string_id call = new string_id(CONVO, "call_in_" + faction);
         chat.chat(trooper, call);
@@ -148,9 +136,6 @@ public class crackdown_cantina extends script.base_script
     }
     public int sendBackup(obj_id self, dictionary params) throws InterruptedException
     {
-        obj_id foyer = getCellId(self, "foyer1");
-        obj_id mainroom = getCellId(self, "cantina");
-        obj_id foyer2 = getCellId(self, "back_hallway");
         obj_id attacker = params.getObjId("attacker");
         location where = params.getLocation("locale");
         if (attacker == null || where == null)
@@ -159,7 +144,7 @@ public class crackdown_cantina extends script.base_script
         }
         obj_id victim = params.getObjId("me");
         String faction = getStringObjVar(victim, "string_faction");
-        String troublemaker = null;
+        String troublemaker;
         if (faction.equals("rebel"))
         {
             troublemaker = "crackdown_rebel_command_security_guard";
@@ -169,24 +154,20 @@ public class crackdown_cantina extends script.base_script
             troublemaker = "crackdown_stormtrooper";
         }
         String planet = where.area;
-        location impLoc = new location(48.13f, .1f, 2.47f, planet, foyer);
-        location impLoc2 = new location(-14.38f, 0, 14.29f, planet, mainroom);
-        location impLoc3 = new location(-17.8f, 0, -11.53f, planet, foyer2);
-        obj_id backupOne = create.object(troublemaker, impLoc);
-        attachScript(backupOne, "city.imperial_crackdown.imperial_backup");
-        setObjVar(backupOne, "whereToFight", where);
-        setObjVar(backupOne, "whoToFight", attacker);
-        setObjVar(backupOne, "string_faction", faction);
-        obj_id backupTwo = create.object(troublemaker, impLoc2);
-        attachScript(backupTwo, "city.imperial_crackdown.imperial_backup");
-        setObjVar(backupTwo, "whereToFight", where);
-        setObjVar(backupTwo, "whoToFight", attacker);
-        setObjVar(backupTwo, "string_faction", faction);
-        obj_id backupThree = create.object(troublemaker, impLoc3);
-        attachScript(backupThree, "city.imperial_crackdown.imperial_backup");
-        setObjVar(backupThree, "whereToFight", where);
-        setObjVar(backupThree, "whoToFight", attacker);
-        setObjVar(backupThree, "string_faction", faction);
+        location[] impLocs = new location[3];
+        impLocs[0] = new location(48.13f, .1f, 2.47f, planet, getCellId(self, "foyer1"));
+        impLocs[1] = new location(-14.38f, 0, 14.29f, planet, getCellId(self, "cantina"));
+        impLocs[2] = new location(-17.8f, 0, -11.53f, planet, getCellId(self, "back_hallway"));
+
+        obj_id backup;
+
+        for (location impLoc : impLocs) {
+            backup = create.object(troublemaker, impLoc);
+            attachScript(backup, "city.imperial_crackdown.imperial_backup");
+            setObjVar(backup, "whereToFight", where);
+            setObjVar(backup, "whoToFight", attacker);
+            setObjVar(backup, "string_faction", faction);
+        }
         return SCRIPT_CONTINUE;
     }
 }
--- sku.0/sys.server/compiled/game/script/city/imperial_crackdown/door_check.java
+++ sku.0/sys.server/compiled/game/script/city/imperial_crackdown/door_check.java
@@ -1,14 +1,7 @@
 package script.city.imperial_crackdown;
 
-import script.*;
-import script.base_class.*;
-import script.combat_engine.*;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.Vector;
-import script.base_script;
-
-import script.library.factions;
+import script.dictionary;
+import script.obj_id;
 
 public class door_check extends script.base_script
 {
