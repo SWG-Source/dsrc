cbd5ef1d77806268a6ea92338a92ce32a2530f53
Cekis <cekisswg@gmail.com>
2016-03-31 04:25:45 +0100

    Made code efficiency changes to the interior conversation spawners and the jawa wander classes.

--- sku.0/sys.server/compiled/game/script/city/interior_4_convo.java
+++ sku.0/sys.server/compiled/game/script/city/interior_4_convo.java
@@ -1,15 +1,7 @@
 package script.city;
 
-import script.*;
-import script.base_class.*;
-import script.combat_engine.*;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.Vector;
-import script.base_script;
-
-import script.library.ai_lib;
-import script.library.create;
+import script.dictionary;
+import script.obj_id;
 
 public class interior_4_convo extends script.city.interior_convo_base
 {
@@ -18,23 +10,17 @@ public class interior_4_convo extends script.city.interior_convo_base
     }
     public int OnInitialize(obj_id self) throws InterruptedException
     {
-        spawnGuyOne(self);
-        spawnGuyTwo(self);
-        spawnGuyThree(self);
-        spawnGuyFour(self);
+        spawnGuy(self,"guy1");
+        spawnGuy(self,"guy2");
+        spawnGuy(self,"guy3");
+        spawnGuy(self,"guy4");
         messageTo(self, "checkForScripts", null, 5, false);
         messageTo(self, "handleChatting", null, 10, false);
         return SCRIPT_CONTINUE;
     }
     public int OnAttach(obj_id self) throws InterruptedException
     {
-        spawnGuyOne(self);
-        spawnGuyTwo(self);
-        spawnGuyThree(self);
-        spawnGuyFour(self);
-        messageTo(self, "checkForScripts", null, 5, false);
-        messageTo(self, "handleChatting", null, 10, false);
-        return SCRIPT_CONTINUE;
+        return OnInitialize(self);
     }
     public int handleChatting(obj_id self, dictionary params) throws InterruptedException
     {
@@ -54,34 +40,6 @@ public class interior_4_convo extends script.city.interior_convo_base
         setAnimationMood(guy2, "conversation");
         setAnimationMood(guy3, "conversation");
         setAnimationMood(guy4, "conversation");
-        return SCRIPT_CONTINUE;
-    }
-    public int handleGuyOneKilled(obj_id self, dictionary params) throws InterruptedException
-    {
-        spawnGuyOne(self);
-        return SCRIPT_CONTINUE;
-    }
-    public int handleGuyTwoKilled(obj_id self, dictionary params) throws InterruptedException
-    {
-        spawnGuyTwo(self);
-        return SCRIPT_CONTINUE;
-    }
-    public int handleGuyThreeKilled(obj_id self, dictionary params) throws InterruptedException
-    {
-        spawnGuyThree(self);
-        return SCRIPT_CONTINUE;
-    }
-    public int handleGuyFourKilled(obj_id self, dictionary params) throws InterruptedException
-    {
-        spawnGuyFour(self);
-        return SCRIPT_CONTINUE;
-    }
-    public int checkForScripts(obj_id self, dictionary params) throws InterruptedException
-    {
-        if (hasScript(self, "theme_park.poi.launch"))
-        {
-            detachScript(self, "theme_park.poi.launch");
-        }
         return SCRIPT_CONTINUE;
     }
 }
