1531248608ed23eaaa4e431da2e6c62fea996a6b
Cekis <cekisswg@gmail.com>
2016-04-10 04:51:40 +0100

    Audited GCW Launch scripts - not clear how these are used or what for though.

--- sku.0/sys.server/compiled/game/script/event/gcwlaunch/gcwlaunch.java
+++ sku.0/sys.server/compiled/game/script/event/gcwlaunch/gcwlaunch.java
@@ -1,18 +1,9 @@
 package script.event.gcwlaunch;
 
-import script.*;
-import script.base_class.*;
-import script.combat_engine.*;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.Vector;
-import script.base_script;
-
-import script.library.utils;
-import script.library.create;
-import script.library.chat;
-import script.library.ai_lib;
+import script.dictionary;
 import script.library.locations;
+import script.location;
+import script.obj_id;
 
 public class gcwlaunch extends script.base_script
 {
