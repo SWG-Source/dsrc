9b9b2b4508a2effb73402e6d08520740982b3ccf
Cekis <cekisswg@gmail.com>
2016-12-07 22:52:47 +0000

    Fixed NPCs in the Bestine Pilots Club with double clothing items added to them.

--- sku.0/sys.server/compiled/game/script/theme_park/tatooine/bestine_pilots_club/pilot6.java
+++ sku.0/sys.server/compiled/game/script/theme_park/tatooine/bestine_pilots_club/pilot6.java
@@ -1,18 +1,7 @@
 package script.theme_park.tatooine.bestine_pilots_club;
 
-import script.*;
-import script.base_class.*;
-import script.combat_engine.*;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.Vector;
-import script.base_script;
-
 import script.library.hue;
-import script.library.colors;
-import script.library.ai_lib;
-import script.library.factions;
-import script.ai.ai;
+import script.obj_id;
 
 public class pilot6 extends script.base_script
 {
@@ -32,7 +21,7 @@ public class pilot6 extends script.base_script
         
         {
             obj_id suit = createObject("object/tangible/wearables/bodysuit/bodysuit_tie_fighter.iff", self, "");
-            obj_id boots = createObject("object/tangible/wearables/boots/boots_s03.iff", self, "");
+            //obj_id boots = createObject("object/tangible/wearables/boots/boots_s03.iff", self, "");
             hue.hueObject(self);
             pvpSetAlignedFaction(self, (-615855020));
             pvpMakeDeclared(self);
