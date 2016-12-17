c77fbd0c44d8b019a548667aa9194e4e63fe7f06
Cekis <cekisswg@gmail.com>
2016-12-17 08:49:22 +0000

    Argh... test before you commit, Cekis!

--- sku.0/sys.server/compiled/game/script/theme_park/dungeon/corvette/death_target.java
+++ sku.0/sys.server/compiled/game/script/theme_park/dungeon/corvette/death_target.java
@@ -39,13 +39,14 @@ public class death_target extends script.base_script
                     obj_id player = (obj_id) players.get(i);
                     messageTo(player, "youWin", null, 1, false);
                     if(!prizeAwarded) {
+                        // the ITV should be a 12.5% chance to be awarded.
                         int prize = rand(0, 7);
                         if (prize == 4) {
                             obj_id inv = utils.getInventoryContainer(player);
                             obj_id itv = createObject("object/tangible/veteran_reward/instant_travel_terminal.iff", inv, "");
                             if (isIdValid(itv)) {
                                 CustomerServiceLog("Loot", "User: (" + player + ") " + getName(player) + " has won the factional Instant Travel Vehicle.");
-                                sendSystemMessage(player, "Congratulations! You've looted the factional Instant Travel Vehicle!");
+                                sendSystemMessage(player, "Congratulations! You've looted the factional Instant Travel Vehicle!", null);
                                 playMusic(player, "sound/music_mission_complete.snd");
                                 prizeAwarded = true;
                             }
