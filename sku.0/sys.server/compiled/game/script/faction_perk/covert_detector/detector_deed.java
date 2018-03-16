f52aebcfb123585030d0ed98723e55ec01fa6056
SWG Source <theswgsource@gmail.com>
2018-03-16 22:43:03 +0000

    temp.txt created online with Bitbucket

--- sku.0/sys.server/compiled/game/script/faction_perk/covert_detector/detector_deed.java
+++
@@ -1,8 +0,0 @@
-package script.faction_perk.covert_detector;
-
-public class detector_deed extends script.faction_perk.base.factional_deed
-{
-    public detector_deed()
-    {
-    }
-}
--- sku.0/sys.server/compiled/game/script/gambling/base/card.java
+++
@@ -1,8 +0,0 @@
-package script.gambling.base;
-
-public class card extends script.gambling.base.default_interface
-{
-    public card()
-    {
-    }
-}
