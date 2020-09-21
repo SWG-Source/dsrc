7ef9f9282253714e6e3a75046261a939e562b565
Geit <rory_king@hotmail.co.uk>
2020-09-21 18:42:29 -0400

    Update from code review comments

--- sku.0/sys.server/compiled/game/script/item/special/no_trade_removable.java
+++ sku.0/sys.server/compiled/game/script/item/special/no_trade_removable.java
@@ -4,6 +4,7 @@ import script.obj_id;
 import script.string_id;
 import script.menu_info;
 import script.menu_info_types;
+import script.library.utils;
 
 /**
  * Allows items that previously had the "noTrade" flag to be made tradeable by
@@ -18,9 +19,13 @@ public class no_trade_removable extends script.base_script {
     // Using one of the unused reserved entries.
     public static final int RADIAL_MENU_ENTRY = menu_info_types.SERVER_MENU47;
     public static final string_id REMOVE_NO_TRADE = new string_id("ui_radial", "no_trade_removable");
+    public static final string_id SID_ITEM_NOT_INVENTORY = new string_id("sarlacc_minigame", "item_not_inventory");
+    public static final string_id SID_ITEM_NOT_OWNER = new string_id("base_player", "item_not_owner");
+    public static final string_id SID_ITEM_MADE_TRADABLE = new string_id("system_msg", "item_made_tradable");
 
     public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException {
-        if (hasObjVar(self, "noTrade") && (getOwner(self) == player || isGod(player))) {
+        if ((getOwner(self) == player || isGod(player) && hasObjVar(self, "noTrade"))
+                && utils.getContainingPlayer(self) == player) {
             mi.addRootMenu(RADIAL_MENU_ENTRY, REMOVE_NO_TRADE);
         }
         return SCRIPT_CONTINUE;
@@ -28,18 +33,20 @@ public class no_trade_removable extends script.base_script {
 
     public int OnObjectMenuSelect(obj_id self, obj_id player, int selectedMenuItem) throws InterruptedException {
         if (selectedMenuItem == RADIAL_MENU_ENTRY) {
-            if (!hasObjVar(self, "noTrade")) {
-                sendSystemMessage(player, "This item is already tradeable.", null);
-                return SCRIPT_CONTINUE;
-            }
-
             if (getOwner(self) != player && !isGod(player)) {
-                sendSystemMessage(player, "This item does not belong to you.", null);
+                sendSystemMessage(player, SID_ITEM_NOT_OWNER);
                 return SCRIPT_CONTINUE;
             }
 
-            removeObjVar(self, "noTrade");
-            sendSystemMessage(player, "This item is now tradable.", null);
+            if (utils.getContainingPlayer(self) != player) {
+                sendSystemMessage(player, SID_ITEM_NOT_INVENTORY);
+                return SCRIPT_CONTINUE;
+            }
+
+            if (hasObjVar(self, "noTrade")) {
+                removeObjVar(self, "noTrade");
+                sendSystemMessage(player, SID_ITEM_MADE_TRADABLE);
+            }
         }
         return SCRIPT_CONTINUE;
     }
