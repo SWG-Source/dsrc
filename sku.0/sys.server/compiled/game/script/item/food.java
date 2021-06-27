4edc48c21922d699e3a832d245924d38dcfe2b32
AconiteX <63141077+AconiteX@users.noreply.github.com>
2021-06-27 01:47:37 -0400

    Missed one

--- sku.0/sys.server/compiled/game/script/item/food.java
+++ sku.0/sys.server/compiled/game/script/item/food.java
@@ -278,12 +278,12 @@ public class food extends script.base_script
                 default:
                 snd += "human_";
             }
-            switch (getGender(player))
+            if(getGender(player) == Gender.FEMALE)
             {
-                case GENDER_FEMALE:
                 snd += "female_eat.cef";
-                break;
-                default:
+            }
+            else
+            {
                 snd += "male_eat.cef";
             }
             playClientEffectLoc(player, snd, getLocation(player), getScale(player));
