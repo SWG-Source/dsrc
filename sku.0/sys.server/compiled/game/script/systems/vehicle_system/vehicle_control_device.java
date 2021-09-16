e88dc114fc4819480794b34705f3d8b20d54164e
AconiteX <63141077+AconiteX@users.noreply.github.com>
2021-09-16 13:32:13 -0400

    Update vehicle_control_device.java
    Small adjustment here to make this more logical, show "Lava Resistance: 100%" attribute if the template already has resistance OR if the lava resistance kit has been used on the vehicle, not just if the kit has been used.

--- sku.0/sys.server/compiled/game/script/systems/vehicle_system/vehicle_control_device.java
+++ sku.0/sys.server/compiled/game/script/systems/vehicle_system/vehicle_control_device.java
@@ -378,7 +378,7 @@ public class vehicle_control_device extends script.base_script
                 return SCRIPT_CONTINUE;
             }
         }
-        if(hasObjVar(self, vehicle.VAR_LAVA_RESISTANT))
+        if(vehicle.isLavaResistant(self))
         {
             names[idx] = "lava_resistance";
             attribs[idx] = "100%";
